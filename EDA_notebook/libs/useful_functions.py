b38a0fd7e024ad1122ea27e4b19435985baa6abf
Pavel Troshenkov <p.s.troshenkov@gmail.com>
2018-11-12 11:43:29 +0300

    fixed regex

--- EDA_notebook/libs/useful_functions.py
+++ EDA_notebook/libs/useful_functions.py
@@ -10,7 +10,7 @@ def clean_text(x, mode):
     if mode == 'numbers':
         return ''.join(r for r in re.findall(r'[0-9]+', str(x)))
     elif mode == 'text':
-        return ''.join(r for r in re.findall(r'[0-9]+', str(x).lower()))
+        return ''.join(r for r in re.findall(r'[a-z]+', str(x).lower()))
     else:
         return 'choose correct mode value [numbers, text]'
 
